915a403515f57e44731db6d08dbd51eeeb50928e
Negry <up240103@alumnos.upa.edu.mx>
2026-03-01 21:14:36 -0600

    Añadiendo contenido a Helper

--- src/Helper.elm
+++ src/Helper.elm
@@ -1,1 +1,58 @@
-module Helper exposing (..)
+module Helper exposing
+    ( AirplaneStatus(..)
+    , GradeStatus(..)
+    , airplaneScheduleAction
+    , airportAction
+    , categoricalGrade
+    )
+
+
+type GradeStatus
+    = Approved
+    | Failed
+    | Pending
+
+
+categoricalGrade : List Float -> List GradeStatus
+categoricalGrade grades =
+    List.map
+        (\grade ->
+            if grade < 0 then
+                Pending
+
+            else if grade >= 7 then
+                Approved
+
+            else
+                Failed
+        )
+        grades
+
+
+type AirplaneStatus
+    = OnTime
+    | Boarding
+    | Delayed
+    | Cancelled
+
+
+airplaneScheduleAction : AirplaneStatus -> String
+airplaneScheduleAction status =
+    case status of
+        Cancelled ->
+            "Pedir reembolso"
+
+        Delayed ->
+            "Esperar"
+
+        OnTime ->
+            "Esperar"
+
+        Boarding ->
+            "Buscar boleto"
+
+
+airportAction : List AirplaneStatus -> List String
+airportAction statuses =
+    List.map airplaneScheduleAction statuses
+
